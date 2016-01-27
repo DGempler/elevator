fb6d13693dc958b0c19a8b95faaf0260f6d0866e
Dmitry Gempler <DGempler@gmail.com>
2016-01-27 15:19:52 -0800

    added floor filter

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 
   angular.module('elevator', [])
 
-    .controller('elevatorController', elevatorController);
+    .controller('elevatorController', elevatorController)
+
+    .filter('floorFilter', floorFilter);
 
     function elevatorController() {
       var vm = this;
 
-      vm.floors = ['Ground', 2, 3, 4, 5, 6, 7, 8, 9];
+      vm.floors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
 
@@ -15,6 +17,17 @@
 
 
 
+    }
+
+    function floorFilter() {
+      return function(floor) {
+        if (floor === 0) {
+          console.log("floor is 0");
+          return "Ground";
+        } else {
+          return floor;
+        }
+      }
     }
 
 })();
